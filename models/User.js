ed50740a1a371082328a3a7adecd0eb26af3483e
geekbhavleen <67952569+geekbhavleen@users.noreply.github.com>
2020-11-07 10:46:50 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  dob: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  shortBio: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('user', UserSchema);
